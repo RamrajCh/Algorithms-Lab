cffdacf0016b3068c30adca467699ed3dc8af01e
RamrajCh <rc02431618@student.ku.edu.np>
2022-04-13 06:56:09 +0545

    Lab 3

--- Lab2/plot.py
+++ Lab2/plot.py
@@ -13,7 +13,6 @@ for x in x_axis_is:
     end_time = time.time()
     y_is_bc.append((end_time-start_time)*10**6)
     
-    
 y_is_wc = []
 for x in x_axis_is:
     values = list(reversed(range(x)))
@@ -22,42 +21,38 @@ for x in x_axis_is:
     end_time = time.time()
     y_is_wc.append((end_time-start_time)*10**6)
     
-
 x_axis_ms = range(100, 10000, 100)
-y_ms_bc = []
+
+y_ms = []
 for x in x_axis_ms:
     values = list(range(x))
     start_time = time.time()
     merge_sort(values, 0, x-1)
     end_time = time.time()
-    y_ms_bc.append((end_time-start_time)*10**6)
+    y_ms.append((end_time-start_time)*10**6)
     
+fig, ax = plt.subplots(nrows=3, figsize=(20,30))
 
-y_ms_wc = []
-for x in x_axis_ms:
-    values = list(reversed(range(x)))
-    start_time = time.time()
-    merge_sort(values, 0, x-1)
-    end_time = time.time()
-    y_ms_wc.append((end_time-start_time)*10**6)
-    
-fig, (insertion_plt, merge_plt) = plt.subplots(nrows=1, ncols=2)
+ax[0].plot(x_axis_is, y_is_bc, '.', label='Best Case - Insertion Sort')
+ax[1].plot(x_axis_is, y_is_wc, 'x', label='Worst Case - Insertion Sort')
 
-insertion_plt.plot(x_axis_is, y_is_bc, '.', label='Best Case')
-insertion_plt.plot(x_axis_is, y_is_wc, 'x', label='Worst Case')
+ax[0].set_xlabel("Length of array (n)")
+ax[0].set_ylabel("Time (in microseconds)")
+ax[0].set_title("Insertion Sort Best Case")
+ax[0].legend()
 
-insertion_plt.set_xlabel("Length of array (n)")
-insertion_plt.set_ylabel("Time (in microseconds)")
-insertion_plt.set_title("Insertion Sort")
-insertion_plt.legend()
+ax[1].set_xlabel("Length of array (n)")
+ax[1].set_ylabel("Time (in microseconds)")
+ax[1].set_title("Insertion Sort Worst Case")
+ax[1].legend()
 
-merge_plt.plot(x_axis_ms, y_ms_bc, '.', label='Best Case')
-merge_plt.plot(x_axis_ms, y_ms_wc, 'x', label='Worst Case')
+ax[2].plot(x_axis_ms, y_ms, '.', label='Merge Sort')
+# plt4.plot(x_axis_ms, y_ms_wc, 'x', label='Worst Case')
 
-merge_plt.set_xlabel("Length of array (n)")
-merge_plt.set_ylabel("Time (in microseconds)")
-merge_plt.set_title("Merge Sort")
-merge_plt.legend()
+ax[2].set_xlabel("Length of array (n)")
+ax[2].set_ylabel("Time (in microseconds)")
+ax[2].set_title("Merge Sort")
+ax[2].legend()
 
 
 plt.show()
